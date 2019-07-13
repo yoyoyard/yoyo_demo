f5dd89c20f22a33418f3979d30d0431c290a0249
Xiaohui Gan <286390860@qq.com>
2019-07-13 10:34:44 +0800

    always use id name as app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import morphdom from 'morphdom'
 
 const mountPoint = document.getElementById('app');
-morphdom(mountPoint, '<div class="hello-world">Hello World</div>');
+morphdom(mountPoint, '<div id="app">Hello World</div>');
